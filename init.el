5936f4b0c8c8b342ce30cc07f88a3cdf466b1f2c
Daniel Kraus <daniel@kraus.my>
2024-08-23 12:02:42 +0200

    Add elpaca cache dir and remove menus

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
 
 ;; Initialize elpaca
 
+(setq elpaca-menu-functions nil)
 (defvar elpaca-installer-version 0.7)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
